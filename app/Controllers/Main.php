47a1b90510a2811ea7e6078cf00341cdc18c5278
shyestfern <zalesak_maxmilian@oauh.cz>
2026-02-04 09:31:13 +0100

    Vytvoření konstrukteru v Main

--- app/Controllers/Main.php
+++ app/Controllers/Main.php
@@ -3,10 +3,19 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
+use Psr\Log\LoggerInterface;
+use App\Models\NavModel;
 
 class Main extends BaseController
 {
+    public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
+    {
+        parent::initController($request, $response, $logger);
+
+    }
+    
     public function index()
     {
         
