a58b7a074d67907c641af2e8c3914a358f85e550
shyestfern <zalesak_maxmilian@oauh.cz>
2026-02-11 09:30:09 +0100

    Na úvodní stránce přidán navbar

--- app/Controllers/Main.php
+++ app/Controllers/Main.php
@@ -6,18 +6,26 @@ use App\Controllers\BaseController;
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 use Psr\Log\LoggerInterface;
-use App\Models\NavModel;
+use App\Models\Okres;
 
 class Main extends BaseController
 {
+    public $data;
+
     public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
     {
         parent::initController($request, $response, $logger);
 
+        $okres = new Okres();
+        $dataOkres = $okres->where('kraj', 141)->findAll();
+
+        $this->data = [
+            "okres" => $dataOkres
+        ];
     }
     
     public function index()
     {
-        
+        echo view('index', $this->data);
     }
 }
---
+++ app/Views/layout/navbar.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-sm bg-primary navbar-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Logo</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+
+                <?php
+                foreach ($okres as $row) {
+                ?>
+
+                    <li class="nav-item">
+                        <?= anchor('okres/'.$row->kod, $row->nazev, ['class'=>'nav-link']) ?>
+                    </li>
+
+                <?php
+                }
+                ?>
+
+            </ul>
+        </div>
+    </div>
+</nav>
