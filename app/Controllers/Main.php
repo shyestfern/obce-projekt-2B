8440cbc6c054883c5ad734d39de118b6efc294f3
shyestfern <zalesak_maxmilian@oauh.cz>
2026-02-18 09:29:02 +0100

    Rozpracování propojení tabulek

--- app/Controllers/Main.php
+++ app/Controllers/Main.php
@@ -7,17 +7,20 @@ use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 use Psr\Log\LoggerInterface;
 use App\Models\Okres;
+use App\Models\Kraj;
+use App\Models\Obec;
 
 class Main extends BaseController
 {
     public $data;
+    public $okres;
 
     public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
     {
         parent::initController($request, $response, $logger);
 
-        $okres = new Okres();
-        $dataOkres = $okres->where('kraj', 141)->findAll();
+        $this->okres = new Okres();
+        $dataOkres = $this->okres->where('kraj', 141)->findAll();
 
         $this->data = [
             "okres" => $dataOkres
@@ -25,12 +28,17 @@ class Main extends BaseController
     }
     
     public function index()
-    {
+    {   
         echo view('index', $this->data);
     }
 
-    public function okres()
+    public function okres($kod)
     {
+        $dataObce = $this->okres->join('obec', 'okres.kod = obec.okres', 'inner')
+            ->join('cast_obce', 'obec.kod = cast_obce.obec', 'inner')
+            ->join('ulice', 'cast_obce.kod = ulice.kod', 'inner')
+            ->join('adresni_misto', '', 'inner');
+
         echo view('okres', $this->data);
     }
 }
